6de8698a074e4a5960870220492a9f8f7cb77764
CK172005 <142142131+CK172005@users.noreply.github.com>
2024-10-28 17:42:28 +0530

    Add files via upload

---
+++ Utilities/read_files.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include "../Utilities/BTree.h"
+using namespace std;
+
+
+long long convertIntoNumber(string num){
+    long long ans=0;
+    string temp;
+    for(int i=0;i<5;i++){
+        temp.push_back(num[i]);
+    }
+    long long firstFive=((stoi)(temp));
+    firstFive=firstFive*1000000;
+    ans+=firstFive;
+    string temp2;
+    for(int i=5;i<11;i++){
+        temp2.push_back(num[i]);
+    }
+    long long lastSixDigit=(stoi)(temp2);
+    ans+=lastSixDigit;
+    return ans;
+}
+
+
+BTree* read_Data() {
+   DIR *dr;
+   struct dirent *en;
+   BTree* root=new BTree();
+   dr = opendir("database"); 
+    if (dr == nullptr) {
+        cerr << "Could not open current directory" << endl;
+        return NULL; // Use EXIT_FAILURE for error exit codes
+    }
+    if (dr) {
+      while ((en = readdir(dr)) != NULL) {
+        if((string)(en->d_name)!="." && (string)(en->d_name)!=".."){
+            string fileName=en->d_name;
+            fileName.erase(11,14);
+            long long number=convertIntoNumber(fileName);
+            root->insert(number);
+        }
+      }
+      closedir(dr); 
+    }
+    return root;
+}
