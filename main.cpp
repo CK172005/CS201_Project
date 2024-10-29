35fc9796470cd414c93ff0ced8a9258240b5bad3
CK172005 <142142131+CK172005@users.noreply.github.com>
2024-10-29 22:15:22 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 #include<bits/stdc++.h>
-#include "./models/User.h"
+#include "./Utilities/write_file.h"
+#include "./models/Transcation.h"
 using namespace std;
+
 
 
 int main(){
     User* newUser=new User("Hello","10/12/2005","10934011","dfjhaoij");
+    User* newUser2=new User("World","12/10/2000","34214209348","djkafkdjfoiw");
+    writeInFile(newUser);
+    writeInFile(newUser2);
+    newUser->depositMoney(10000);
+
+    newUser->transcation_history=new Transcation();
+    newUser2->transcation_history=new Transcation();
+
+    do_Transcation(1000,newUser,newUser2);
+    do_Transcation(500,newUser2,newUser);
+
+    cout<<"New User 2 Transcation"<<endl;
+    newUser2->transcation_history->displayTranscation();
+    cout<<"New user Transcation"<<endl;
+    newUser->transcation_history->displayTranscation();
+    
 }
+
 
