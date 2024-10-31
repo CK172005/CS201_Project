4ae91a7a490610bd79133d51f762fa9ea6174b64
CK172005 <142142131+CK172005@users.noreply.github.com>
2024-10-31 22:35:23 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,20 +8,35 @@ using namespace std;
 
 
 int main(){
+
+    
     // registerUser();
-    // registerUser();
+
+    // User* user1=getUser("17409488245");
+    // cout<<user1->name<<endl;
+    // cout<<user1->password<<endl;
+    // cout<<user1->Address<<endl;
+    // cout<<user1->Account_Number<<endl;
+    // user1->transcation_history->displayTranscation();
 
 
     // User* user1=loginUser();
+    // cout<<user1->name<<endl;
+    // cout<<user1->password<<endl;
+    // cout<<user1->Address<<endl;
+    // cout<<user1->Account_Number<<endl;
     // cout<<user1->phone_Number<<endl;
+    // cout<<user1->dob<<endl;
+    // cout<<user1->age<<endl;
+    // user1->transcation_history->displayTranscation();
 
-    // string accountNumber;
-    // cin>>accountNumber;
-    // User* user1=getUser(accountNumber);
-    // string otherAccountNumber;
-    // cin>>otherAccountNumber;
-    // User* user2=getUser(otherAccountNumber);
+
+    // User* user1=getUser("17409488245");
+    // User* user2=getUser("23221685761");
     // user1->depositMoney(10000);
+
+
+
     // do_Transcation(3000,user1,user2);
     // do_Transcation(1000,user2,user1);
 
