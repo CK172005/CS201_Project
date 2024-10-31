b002449d94ab58799a99cebf9877294a196af65f
CK172005 <142142131+CK172005@users.noreply.github.com>
2024-10-31 21:29:31 +0530

    Update write_file.h

--- Utilities/write_file.h
+++ Utilities/write_file.h
@@ -3,20 +3,22 @@
 
 #include<bits/stdc++.h>
 #include "../models/User.h"
+#include "../Utilities/encryptDecrypt.h"
 using namespace std;
 
 void writeInFile(User* new_user){
-    // Adding the user to the database.
+    //) Adding the user to the database.
     string fileName="database/"+ new_user->Account_Number + ".txt";
     ofstream outputFile(fileName);
     if (outputFile.is_open()) {
-        outputFile << "Name: " << new_user->name << "\n";
-        outputFile << "DOB: " << new_user->dob << "\n";
-        outputFile << "Phone Number: " << new_user->phone_Number << "\n";
-        outputFile << "Address: " << new_user->Address << "\n";
-        outputFile << "Account Number: " << new_user->Account_Number << "\n";
-        outputFile << "Age: "<<new_user->age<<endl;
-        outputFile << "Balance: "<<new_user->account_balance<<endl;
+        outputFile << "Name: " << encodeMessage(new_user->name,new_user->Account_Number) << "\n";
+        outputFile << "DOB: " << encodeMessage(new_user->dob,new_user->Account_Number) << "\n";
+        outputFile << "Phone Number: " << encodeMessage(new_user->phone_Number,new_user->Account_Number) << "\n";
+        outputFile << "Address: " << encodeMessage(new_user->Address,new_user->Account_Number) << "\n";
+        outputFile << "Account Number: " << encodeMessage(new_user->Account_Number,new_user->Account_Number) << "\n";
+        outputFile << "Age: "<<encodeMessage((to_string)(new_user->age),new_user->Account_Number)<<endl;
+        outputFile << "Password: "<<encodeMessage(new_user->password,new_user->Account_Number)<<endl;
+        outputFile << "Balance: "<<encodeMessage((to_string)(new_user->account_balance),new_user->Account_Number)<<endl;
 
         outputFile.close();
     } else {
